97a54b684ec4c0e0679c041fc98fd9678675c329
misch038 <michael.scherbina038@stud.fh-dortmund.de>
2020-05-03 13:20:52 +0200

    Initial commit

---
+++ src/main/java/de/fhdo/swt/aktivreisen24/Aktivreisen24Application.java
@@ -0,0 +1,13 @@
+package de.fhdo.swt.aktivreisen24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aktivreisen24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aktivreisen24Application.class, args);
+    }
+
+}
---
+++ src/test/java/de/fhdo/swt/aktivreisen24/Aktivreisen24ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhdo.swt.aktivreisen24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aktivreisen24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
