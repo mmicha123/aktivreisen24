694b87467a53b1cff437e015bae77bb41dd00e07
misch038 <michael.scherbina038@stud.fh-dortmund.de>
2020-05-11 15:29:41 +0200

    added account model data

--- src/main/java/net/aktivreisen24/model/Account.java
+++ src/main/java/net/aktivreisen24/model/Account.java
@@ -1,5 +1,29 @@
 package net.aktivreisen24.model;
 
 public class Account {
+    private final long acc_id;
+    private final String passhash;
+    private String email;
 
+    public long getAcc_id() {
+        return acc_id;
+    }
+
+    public String getPasshash() {
+        return passhash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Account(long acc_id, String passhash, String email) {
+        this.acc_id = acc_id;
+        this.passhash = passhash;
+        this.email = email;
+    }
 }
