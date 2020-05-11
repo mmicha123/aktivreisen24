13f16c52acd29373373e22f7bcfc9db965cb7304
misch038 <michael.scherbina038@stud.fh-dortmund.de>
2020-05-11 15:23:04 +0200

    changed user

--- src/main/java/net/aktivreisen24/model/User.java
+++ src/main/java/net/aktivreisen24/model/User.java
@@ -1,24 +1,54 @@
 package net.aktivreisen24.model;
 
-import java.util.UUID;
-
 public class User {
 
-    private final String name;
-    private final UUID id;
+    private final long user_id;
+    private final String first_name;
+    private final String last_name;
+    private String country;
+    private String address;
+    private int phone;
 
-    public User(UUID id, String name){
-        this.id = id;
-        this.name = name;
+    public long getUser_id() {
+        return user_id;
     }
 
-    public UUID getId(){
-        return id;
+    public String getFirst_name() {
+        return first_name;
     }
 
-    public String getName(){
-        return name;
+    public String getLast_name() {
+        return last_name;
     }
 
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public User(long user_id, String first_name, String last_name) {
+        this.user_id = user_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
 
 }
