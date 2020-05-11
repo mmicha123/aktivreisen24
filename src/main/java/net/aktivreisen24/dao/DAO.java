b223b716573eb4a79d128b9a4bb52ecd21c11458
misch038 <michael.scherbina038@stud.fh-dortmund.de>
2020-05-11 15:31:41 +0200

    removed getAll

--- src/main/java/net/aktivreisen24/dao/DAO.java
+++ src/main/java/net/aktivreisen24/dao/DAO.java
@@ -5,8 +5,7 @@ import java.util.Optional;
 
 public interface DAO<T> {
     Optional<T> get(int id);
-    List<T> getAll();
-    void save(T t);
+    void insert(T t);
     void update(T t, String[] param);
     void delete(T t);
 }
